028918bf1b5c9e48c3377c1bc8728fb5662628da
igor <igorpaulino0945@gmail.com>
2026-04-24 10:14:02 -0300

    Finalizado a parte de Ingestão dos dados

--- extracao.py
+++ extracao.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from pyspark.sql  import functions as F
 from pyspark.sql import SparkSession  
-from pyspark.sql.types import StructType, StructField, StringType, FloatType, IntegerType  
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType  
 import json
 
 load_dotenv()
@@ -28,6 +28,40 @@ def extracao():
         print(f"Erro ao acessar a URL: {e}")
     
 
-extracao()    
+
+def ingestion_raw(df):
+    try:
+        schema_rating= StructType([
+            StructField("count", IntegerType(),True),
+            StructField("rate", DoubleType(), True)
+
+        ])
+
+        schema= StructType([
+            StructField("category", StringType(), True),
+            StructField("description", StringType(), True),
+            StructField("id", IntegerType(), True),
+            StructField("image", StringType(), True),
+            StructField("price", DoubleType(), True),
+            StructField("rating", schema_rating, True),
+            StructField("title", StringType(), True)
+
+        ])
+        df=spark.read.json(spark.sparkContext.parallelize([dados]), schema=schema)
+        print(df.printSchema())
+
+        df= df.withColumn("avaliacoes", F.col("rating.count")) \
+                        .withColumn("rate", F.col("rating.rate")) \
+                        .drop("rating")
+
+        df.write\
+        .mode("overwrite")\
+        .parquet("raw_data/dados.parquet")
+        print("✅ Ingestão bem sucedida!")
+    except Exception as e:
+        print(f"Erro ao salvar os dados: {e}")
+
+
 dados = extracao()
-df=spark.read.json(spark.sparkContext.parallelize([dados]))
+ingestion_raw(dados)
+
